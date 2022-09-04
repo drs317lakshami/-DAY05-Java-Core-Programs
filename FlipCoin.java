dadd5515138cfc9542e6c6296cdfe513456dda26
drs317lakshami <devrajkumar317@gmail.com>
2022-09-04 15:13:43 +0530

    ADDING: Welcome To Flip Coin Programs

---
+++ FlipCoin.java
@@ -0,0 +1,16 @@
+package bridgelabz.day05.com;
+
+public class FlipCoin 
+{
+    public static void main(String args[])
+    {
+
+        if(Math.random() < 0.5 ) {
+            System.out.println("Heads");
+        }
+        else{
+            System.out.println("Tails");
+        }
+
+    }
+}
